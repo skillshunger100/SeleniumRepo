cc143ec5454b3365f3f24c8adff17ac6daa1dfde
Debdas <skillshunger47@gmail.com>
2026-01-18 20:59:48 +0530

    Again commit from XMan

---
+++ src/main/java/Selenium/Selenium/Api.java
@@ -0,0 +1,10 @@
+package Selenium.Selenium;
+
+public class Api {
+	
+	private void SYSO() {
+	   System.out.println("Hello I'm API");
+
+	}
+
+}
---
+++ src/main/java/Selenium/Selenium/Facebook.java
@@ -0,0 +1,9 @@
+package Selenium.Selenium;
+
+public class Facebook {
+
+	
+	private void SYSO() {
+		System.out.println("Hello im facebook here");
+	}
+}
