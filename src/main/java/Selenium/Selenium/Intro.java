faf8cb598e5999bdd5c9a26a1c818f07ccf6a93a
Debdas <skillshunger47@gmail.com>
2026-01-18 20:31:40 +0530

    commit from XMan

--- src/main/java/Selenium/Selenium/Intro.java
+++ src/main/java/Selenium/Selenium/Intro.java
@@ -2,24 +2,26 @@ package Selenium.Selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Intro {
-	
+
 	WebDriver driver = null;
-	
+
 	@Test
 	public void base() {
-		
+
 		WebDriverManager.chromedriver().setup();
-		
+
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
-		
+
 	}
-	
+
+	@AfterTest
 	public void leave() {
 		driver.close();
 	}
